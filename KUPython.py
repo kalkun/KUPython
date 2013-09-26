67262a03080181216f8ccf618057457aea646db1
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2013-09-26 20:50:10 +0200

    Update KUPython.py

--- KUPython.py
+++ KUPython.py
@@ -1,17 +1,17 @@
 #########################################################################
-#									#
-#	KUPython is a module intended to help building			#
-#	applications for KU's Absalon in Python 			#
-#									#
-#	In order for KUPython to work Requests need to be 		#
-#	installed. 							#
-#									#
-#	link to install Requests:					#
-#	http://docs.python-requests.org/en/latest/user/installed 	#
-#									#
-#									#
-#									#
-#									#
+#																		#
+#	KUPython is a module intended to help building						#
+#	applications for KU's Absalon in Python 							#
+#																		#
+#	In order for KUPython to work Requests need to be 					#
+#	installed. 															#
+#																		#
+#	link to install Requests:											#
+#	http://docs.python-requests.org/en/latest/user/installed 			#
+#																		#
+#																		#
+#																		#
+#																		#
 #########################################################################
 
 from requests import Session
