2b08d96f727587e0d636c1f134e4b467ec8bb34f
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2013-09-27 03:41:37 +0200

    minor change

--- KUPython.py
+++ KUPython.py
@@ -1,13 +1,13 @@
 #########################################################################
 #                                                                       #
-#	KUPython is a module intended to help building                      #
-#	applications for KU's Absalon in Python                             #
+#   KUPython is a module intended to help building                      #
+#   applications for KU's Absalon in Python                             #
 #                                                                       #
-#	In order for KUPython to work Requests need to be                   #
-#	installed.                                                          #
+#   In order for KUPython to work Requests need to be                   #
+#   installed.                                                          #
 #                                                                       #
-#	link to install Requests:                                           #
-#	http://docs.python-requests.org/en/latest/user/installed            #
+#   link to install Requests:                                           #
+#   http://docs.python-requests.org/en/latest/user/installed            #
 #                                                                       #
 #                                                                       #
 #                                                                       #
