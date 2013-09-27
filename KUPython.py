91f402679459d7b6d436612acccda40eb8a5ae3a
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2013-09-27 03:36:55 +0200

    fixed indentation

--- KUPython.py
+++ KUPython.py
@@ -14,11 +14,14 @@
 #																		#
 #########################################################################
 
-from requests import Session
+import requests
+import re
 
 
 class absalonSession:
-	""" A class to begin a user session on KU's studentnet Absalon """
+	""" A class to begin a user session on KU's studentnet Absalon 
+		as well as providing some basic navigation on the site	"""
+
 	def __init__(self, 
 		username, 
 		password, 
@@ -33,12 +36,66 @@ class absalonSession:
 			'password' : password, 
 			'curl' : curl,
 			'flags' : flags,
-			'forcedownlevel' : forcedownlevel
+			'forcedownlevel' : forcedownlevel,
 			'formdir' : formdir
 		}
 
 		# assign a request session object to self
 		self.session = requests.Session()
 
-	def login(self):
-		return self.session.post("https://pabsws.ku.dk/CookieAuth.dll?Logon", data=params)
+		# start user session
+		tmp = self.session.post("https://pabsws.ku.dk/CookieAuth.dll?Logon", data=self.params)
+
+		# self.courses is a list of course IDS for the current user
+		self.courses = re.findall('(?<=CourseID=)[0-9]+', tmp.text.encode('utf-8'))
+
+		self.coursefolders = {}
+
+	def coursefolder(self, 
+		courseID):
+		# go to course page of a given courseID
+		
+		#bind parameters
+		params = {'CourseID' : courseID}
+
+		# execute GET
+		page = self.session.request('GET', "https://absalon.itslearning.com/main.aspx", params=params).text.encode('utf-8')
+
+		#search for the course Folder ID and bind it to params
+		params = { 'FolderID' : re.search('(?<=FolderID=)[0-9]+', page).group() }
+		
+		folder = self.session.request('GET', 'https://absalon.itslearning.com/Folder/processfolder.aspx', params)
+
+		return folder
+
+	def getassignmentsfolder(self, coursefolder):
+		# given a coursefolder it finds the assignment folder in it
+		return None
+
+	def getassignmentids(self, 
+		FolderID): 
+		# Given a FolderID getassignmentids looks up EssayID which is essentially to get 
+		# pages of each specific assignment for the course. 
+
+		params = {'FolderID' : FolderID}
+		overview = self.session.request("GET", "https://absalon.itslearning.com/Folder/processfolder.aspx", params=params)
+		
+		return re.findall('(?<=EssayID=)[0-9]+', overview.text.encode('utf-8'))
+
+	def checkassignments(self,
+		folderid):
+		# given an assignment folder it checks all the assignment pages to check their status
+
+		folderlist = self.getassignmentids(folderid)
+		allStatus = []
+		for assigns in folderlist:
+			params = {'EssayID' : assigns}
+			lookup = self.session.request("GET", "https://absalon.itslearning.com/essay/read_essay.aspx", params=params)
+			status = re.search('(?<=<th>Status</th>\r\n\t\t\t\t).*?</td>', lookup.text.encode('utf-8'), flags=re.DOTALL)
+			if status != None:
+				allStatus.extend([status.group()])
+			#status = re.search('(?<=<th>Status</th>)^<td>$</td>', check)
+		return allStatus
+
+
+
--- kulogin.py
+++ kulogin.py
@@ -1,3 +1,5 @@
+## not really anything for the long run
+
 import requests
 import re
 
@@ -13,12 +15,24 @@ params = { 	'username' : 'tgw831',
 r = s.post("https://pabsws.ku.dk/CookieAuth.dll?Logon", data=params) #, cookies=cookie)
 
 courseID1 = {'LocationID' : 49964, "LocationType" : 1, "FolderID" : 2062228}
-#HCI = s.request("POST" ,"https://absalon.itslearning.com/ContentArea/ContentArea.aspx", params=courseID1)
+HCI = s.get("https://absalon.itslearning.com/ContentArea/ContentArea.aspx", params=courseID1)
+#HCI = requests.get("http://rebook2.rebook.dk")
+
+pivot = re.compile('Diskussion af usability testresultater med kunde')
+dump = open('dump.html', 'w')
+dump.write(HCI.text.encode('utf-8'))
+dump.close()
+
+print len(pivot.findall(HCI.text)) > 0 
+print HCI.url
+
+courses = re.compile 
+
 #print HCI.url
 
-Opgave5 = s.request('GET', 'https://absalon.itslearning.com/essay/read_essay.aspx?EssayID=2071783', data=params)
+#Opgave5 = s.request('GET', 'https://absalon.itslearning.com/essay/read_essay.aspx?EssayID=2071783') # , data=params
 
-print Opgave5.text
+#rint r.text
 
 #assignments = re.compile('EssayID=[0-9]+')
 
