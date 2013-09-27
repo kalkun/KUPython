53999d5d1e235a8926440d070abe360d477997ea
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2013-09-27 12:58:25 +0200

    added some info on the class

--- KUPython.py
+++ KUPython.py
@@ -1,4 +1,18 @@
-
+#########################################################################
+#                                                                       #
+#   KUPython is a module intended to help building                      #
+#   applications for KU's Absalon in Python                             #
+#                                                                       #
+#   In order for KUPython to work Requests need to be                   #
+#   installed.                                                          #
+#                                                                       #
+#   link to install Requests:                                           #
+#   http://docs.python-requests.org/en/latest/user/installed            #
+#                                                                       #
+#                                                                       #
+#                                                                       #
+#                                                                       #
+#########################################################################
 
 import requests
 import re
